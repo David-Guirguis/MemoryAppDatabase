a0f095b3a739b93859f00cc37acbc6b734059397
Mrjieejiee <38105868+Mrjieejiee@users.noreply.github.com>
2020-12-18 12:22:17 +0100

    Update CreateDB.sql

--- CreateDB.sql
+++ CreateDB.sql
@@ -3,23 +3,22 @@ use memoryApp;
 
 create table if not exists t_user (
 	userID int primary key AUTO_INCREMENT,
-    googleID varchar(64)
+    	googleID varchar(64)
 );
 
 create table if not exists t_memory (
 	memoryID int primary key,
 	userID int not null,
-    memorytitle varchar(128),
+	memorytitle varchar(128),
 	memorybody varchar(2048),
-    
+
 	foreign key(userID) references t_user(userID)
 );
 
 create table if not exists t_tag (
 	tagID int primary key,
-    tag varchar(64),
-    memoryID int,
+    	tag varchar(64),
+    	memoryID int,
     
-    foreign key(memoryID) references t_memory(memoryID)
+    	foreign key(memoryID) references t_memory(memoryID)
 );
-
